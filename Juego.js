c67fafd52635d77d2cc64e6c0725c5e80aef3545
Juan Zabala <juanpablozabla@gmail.com>
2025-03-11 11:39:36 -0500

    juego

---
+++ Juego.js
@@ -0,0 +1,17 @@
+
+function getRandomNumber() {
+
+    const random = Math.random()
+    const multiplied = random * 10 
+    const rounded = Math.floor(multiplied)
+    const result = rounded + 1
+    return result
+}
+console.log (getRandomNumber())
+let numero = parseInt(prompt("Elige un numero del 1 al 10"));
+let numero1 = result;
+
+  if (numero = numero1){
+    alert = ("Felicidades haz acertado")
+}
+
